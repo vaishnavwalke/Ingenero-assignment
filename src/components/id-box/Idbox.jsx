3a95edf30d44acf2213a86c67889fa1c7e11a36f
vaishnavwalke <vaishnavwalke2@gmail.com>
2025-08-14 22:14:45 +0530

    first part done

--- src/components/id-box/Idbox.jsx
+++ src/components/id-box/Idbox.jsx
@@ -1,9 +1,9 @@
 import "./Idbox.css";
-const Idbox = ({ id, handleIdSelect, selectedId }) => {
+const Idbox = ({ id, handleIdSelect, selectedId, isSelected }) => {
   return (
     <div
       onClick={() => handleIdSelect(id)}
-      className={`box ${selectedId === id ? "selected" : ""}`}
+      className={`box ${selectedId === id || isSelected ? "selected" : ""}`}
     >
       {id}
     </div>
--- src/components/main-container/parts-list/PartsList.jsx
+++ src/components/main-container/parts-list/PartsList.jsx
@@ -3,15 +3,33 @@ import "./PartsList.css";
 import { element_ids } from "../../../data.js";
 import Idbox from "../../id-box/Idbox";
 const PartsList = ({ selectedPartIds, setSelectedPartIds }) => {
+  const handleCheckboxChange = (id) => {
+    if (selectedPartIds?.includes(id)) {
+      setSelectedPartIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setSelectedPartIds((prev) => [...prev, id]);
+    }
+  };
+  const handleSelectAll = () => {
+    let allIds = element_ids?.map((item) => item.id);
+    if (
+      selectedPartIds?.every((item, index) => item === allIds[index]) &&
+      selectedPartIds?.length !== 0
+    ) {
+      setSelectedPartIds([]);
+    } else {
+      setSelectedPartIds(allIds);
+    }
+  };
   return (
     <div className="part-list">
       <div className="heading">
         <div>Position</div>
         <div>Elements Part ID</div>
-        <button>Select All</button>
+        <button onClick={handleSelectAll}>Select All</button>
       </div>
       <div className="part-data">
-        {element_ids.map((item) => {
+        {element_ids?.map((item) => {
           return (
             <div key={item.id} className="heading gap">
               <div>{item?.position}</div>
@@ -20,10 +38,15 @@ const PartsList = ({ selectedPartIds, setSelectedPartIds }) => {
                   name={item.id}
                   id={item.id}
                   type="checkbox"
-                  checked={""}
-                  onChange={() => {}}
-                />{" "}
-                <Idbox id={item?.id} handleIdSelect={() => {}} />
+                  checked={selectedPartIds?.includes(item.id) ? true : false}
+                  onChange={() => handleCheckboxChange(item.id)}
+                  className="checkbox"
+                />
+                <Idbox
+                  id={item?.id}
+                  handleIdSelect={() => {}}
+                  isSelected={selectedPartIds?.includes(item.id)}
+                />
               </div>
             </div>
           );
